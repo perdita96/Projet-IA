7bcb94c7f052b9daadc926861b51a19bc815bf67
Romain Marchand <etu51203@henallux.be>
2024-10-09 12:15:20 +0200

    set up file DB

---
+++ initDB.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from BLOCKADE.models import db, init_db  # Importer votre base de données et la fonction init_db
+from BLOCKADE.views import app           # Importer votre application Flask
+import config
+
+# Associez la base de données à l'application Flask
+db.init_app(app)
+
+# Contexte de l'application Flask (car en dehors de blockade)
+with app.app_context():
+    # Appel à la fonction init_db() pour initialiser la base de données
+    init_db()
