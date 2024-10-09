0b830436589d046e8574d9d1fa83769fbb42d30f
Romain Marchand <etu51203@henallux.be>
2024-10-09 21:42:51 +0200

    maj

--- BLOCKADE/views.py
+++ BLOCKADE/views.py
@@ -36,12 +36,13 @@ def id_searched_player(nickname):
 #précondition : le pseudo d'un joueur qui n'est pas encore présent dans la base de données est fourni en argument
 #postcondition : le joueur est rajouté à la base de données
 def add_player(nickname): 
-    new_player = Player(is_human=True, nickname=nickname) 
+    new_player = Player(is_human= nickname!= 'AI', nickname=nickname) 
     db.session.add(new_player)  
     db.session.commit()
 
 #précondition : les pseudos de deux joueurs sont données en argument même si il n'existe pas dans la DB. La taille de la grille du jeu peut aussi être donnée
 #postcondition : une partie de la taille passée en argument ou de 5X5 par défaut est créée avec les joueurs passés en arguments
+@app.route('/createGame/<player_1_nickname>/', methods=['GET'])
 @app.route('/createGame/<player_1_nickname>/<player_2_nickname>', methods=['GET'])
 def create_game(player_1_nickname, player_2_nickname='IA', size=config.BOARD_SIZE) :
     if not player_exists(player_1_nickname) :
@@ -52,7 +53,7 @@ def create_game(player_1_nickname, player_2_nickname='IA', size=config.BOARD_SIZ
     player_1_id = id_searched_player(player_1_nickname)
     player_2_id = id_searched_player(player_2_nickname)
 
-    new_game = Game(player_1=player_1_id, player_2=player_2_id, size=size)
+    new_game = Game(player_1=player_1_id, player_2=player_2_id, size=config.BOARD_SIZE)
     db.session.add(new_game)
     db.session.commit()
 
@@ -66,7 +67,7 @@ def create_game(player_1_nickname, player_2_nickname='IA', size=config.BOARD_SIZ
         'winner_player_1' : new_game.winner_player_1
     }
     """
-    return redirect(url_for('game', game_state=game, player_id=player_1_id))  
+    return redirect(url_for('game', game_state=new_game, player_id=player_1_id))  
     #return jsonify(game_state)
     
 
