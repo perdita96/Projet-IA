74f8f89741b8bbcc89cb9a75b2994a5933c127ea
LucienBuret <lucien.buret@gmail.com>
2024-10-30 17:46:29 +0100

    Clean Code

--- BLOCKADE/business.py
+++ BLOCKADE/business.py
@@ -1,10 +1,9 @@
-
 from math import sqrt
 
 
 def move(game, player, direction):
     """
-    Pré-conditions
+    Pré-conditions :
         game est un objet Game valide
         player est un ID de joueur 
         direction est une chaîne
@@ -23,56 +22,59 @@ def move(game, player, direction):
     size = game.size
 
     directions = ['ArrowUp','ArrowDown','ArrowLeft','ArrowRight']
-    if direction in directions:
-        if int(player) == player_1.player_id:
-            current_player = "1"
-            current_player_pos = game.pos_player_1
-        elif int(player) == player_2.player_id:
-            current_player = "2"
-            current_player_pos = game.pos_player_2
-        else:
-            raise ValueError("Player is not in the game")
-
-        if (game.turn_player_1 and current_player == "1") or (not game.turn_player_1 and current_player == "2"):
-            x, y = map(int, current_player_pos.split(","))
-            if direction == "ArrowUp":
-                new_x, new_y = x - 1, y
-            elif direction == "ArrowDown":
-                new_x, new_y = x + 1, y
-            elif direction == "ArrowLeft":
-                new_x, new_y = x, y - 1
-            elif direction == "ArrowRight":
-                new_x, new_y = x, y + 1
-            else:
-                raise ValueError("Direction non valide")
-            if (is_within_board(new_x, new_y , size)):
-                target_case = board_state[new_x * size + new_y]
-                if (target_case == "0" or target_case == current_player):
-                    if current_player == "1":
-                        game.pos_player_1 = f"{new_x},{new_y}"
-                    else:
-                        game.pos_player_2 = f"{new_x},{new_y}"
-
-                    board_state = list(board_state)
-                    board_state[new_x * size + new_y] = current_player
-                    game.board_state = "".join(board_state)
-                    game.turn_player_1 = not game.turn_player_1
-                    if "0" not in board_state:
-                        game.winner_player_1 = board_state.count("1") > size ** 2 // 2
-                    return game
-                else:
-                    raise ValueError("Mouvement non autorisé")
-            else:
-                raise ValueError("Mouvement en dehors du plateau")
-        else:
-            raise ValueError("Ce n'est pas au tour du joueur")
-    else:
+    if direction not in directions:
         raise ValueError("Direction non valide")
+    
+    if int(player) == player_1.player_id:
+        current_player = "1"
+        current_player_pos = game.pos_player_1
+    elif int(player) == player_2.player_id:
+        current_player = "2"
+        current_player_pos = game.pos_player_2
+    else:
+        raise ValueError("Player is not in the game")
+
+    if (not game.turn_player_1 and current_player == "1") or (game.turn_player_1 and current_player == "2"):
+        raise ValueError("Ce n'est pas au tour du joueur")
+    
+    x, y = map(int, current_player_pos.split(","))
+    match direction:
+        case "ArrowUp":
+            new_x, new_y = x - 1, y
+        case "ArrowDown":
+            new_x, new_y = x + 1, y
+        case "ArrowLeft":
+            new_x, new_y = x, y - 1
+        case "ArrowRight":
+            new_x, new_y = x, y + 1
+        case _:
+            raise ValueError("Direction non valide")
+                
+    if (not is_within_board(new_x, new_y , size)):
+        raise ValueError("Mouvement en dehors du plateau")
+    
+    target_case = board_state[new_x * size + new_y]
+    if (target_case != "0" and target_case != current_player):
+        raise ValueError("Mouvement non autorisé")
+    
+    if current_player == "1":
+        game.pos_player_1 = f"{new_x},{new_y}"
+    else:
+        game.pos_player_2 = f"{new_x},{new_y}"
+
+    board_state = list(board_state)
+    board_state[new_x * size + new_y] = current_player
+    game.board_state = "".join(board_state)
+    game.turn_player_1 = not game.turn_player_1
+
+    if "0" not in board_state:
+        game.winner_player_1 = board_state.count("1") > size ** 2 // 2
+    
+    return game
 
 
 def is_within_board(x, y, board_size) :
     return (0 <= x < board_size) and (0 <= y < board_size)
-
 
 def is_move_valid(player_number, case_state) : 
     return player_number == case_state
@@ -95,12 +97,12 @@ def reachable_cases(opponent_number, opponent_pos, board_state) :
         neighbor_case = [todo - side_size, todo + side_size, todo + 1, todo - 1]
 
         i_neighbor = 0
-        while(neighbor_case[i_neighbor] < 4) :
+        while neighbor_case[i_neighbor] < 4 :
             i_case = neighbor_case[i_neighbor]
-            if i_case < board_size and i_case >= 0 and not reachable[i_case] and box_is_accessible(board_state, i_case, opponent_number) : 
+            if 0 <= i_case < board_size and not reachable[i_case] and box_is_accessible(board_state, i_case, opponent_number) : 
                 reachable[i_case] = True
                 queue.append(i_case)
 
-            i_case +=1
+            i_case += 1
 
     return reachable
