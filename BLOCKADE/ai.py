874745b839f446d60438e7fcdd1e22c0c2858f0b
perdita96 <etu51742@henallux.be>
2024-11-18 14:59:33 +0100

    a debug

--- BLOCKADE/ai.py
+++ BLOCKADE/ai.py
@@ -1,13 +1,18 @@
 import random
+from .app_models.models import *
+import config
+
 
 def possible_move(game, player_id):
     """
-    Fonction qui revoir les mouvements possible pour l'IA
+    Fonction qui renvoie les mouvements possibles pour l'IA.
+    
     Pré-conditions :
-        - game est la game en cour
-        - L'id du joueur qui fait le mouvement doit être l'id d'un joueur présent dans la game
+        - game : instance de la classe Game représentant l'état actuel de la partie.
+        - player_id : identifiant du joueur qui effectue le mouvement, doit être présent dans la partie.
+    
     Post-conditions : 
-        Retourne la liste des mouvements valides
+        Retourne une liste des mouvements valides (Up, Down, Left, Right) que le joueur peut effectuer.
     """
     board_state = game.board_state
     size = game.size
@@ -20,15 +25,15 @@ def possible_move(game, player_id):
 
     x, y = map(int, current_pos.split(","))
     possible_move = []
-    for move in ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']:
+    for move in ['Up', 'Down', 'Left', 'Right']:
         match move:
-            case "ArrowUp":
+            case "Up":
                 new_x, new_y = x - 1, y
-            case "ArrowDown":
+            case "Down":
                 new_x, new_y = x + 1, y
-            case "ArrowLeft":
+            case "Left":
                new_x, new_y = x, y - 1
-            case "ArrowRight":
+            case "Right":
                 new_x, new_y = x, y + 1
         if((0 <= new_x < size) and (0 <= new_y < size)): 
             target_square = board_state[new_x * size + new_y]
@@ -38,13 +43,151 @@ def possible_move(game, player_id):
 
 def get_move(game, player_id):
     """
-    Fonction qui revoir le mouvement choisit par l'IA
+    Fonction qui renvoie le mouvement choisi par l'IA et met à jour les données d'apprentissage.
+    
     Pré-conditions :
-        - game est la game en cour
-        - L'id du joueur qui fait le mouvement doit être l'id d'un joueur présent dans la game
+        - game : instance de la classe Game représentant l'état actuel de la partie.
+        - player_id : identifiant du joueur qui effectue le mouvement, doit être présent dans la partie.
+    
     Post-conditions : 
-        Retourne un mouvement valide sur la plateau du jeu
+        Retourne un mouvement valide sur le plateau du jeu sous la forme d'une chaîne de caractères, par exemple "ArrowUp".
+        Met à jour les données d'apprentissage, c'est a dire la Q-Table et previous_state_move dans la base de données
+
+    """
+    previous_state_move = db.session.query(PreviousStateAction).filter_by(game_id=game.game_id, player_id=player_id).first()
+    current_state = state(game)
+    if(previous_state_move):
+        current_player_number = 1 if player_id == game.player_1.player_id else 2
+        update_q_table(previous_state_move, current_state, current_player_number)
+    else:
+        previous_state_move = PreviousStateAction(
+            game_id=game.game_id,
+            player_id=player_id,
+            previous_state=None,
+            previous_action=None)
+        db.session.add(previous_state_move)
+    if(random.random() < config.EPS):
+        move = explore(game, player_id)
+    else:
+        move = exploit(game, player_id, current_state)
+    previous_state_move.previous_state = current_state
+    previous_state_move.previous_action = move
+    db.session.commit()
+    return "Arrow" + move
+
+def explore(game, player_id):
+    """
+    Fonction qui renvoie le mouvement choisi par l'IA lors de la phase d'exploration.
+    
+    Pré-conditions :
+        - game : instance de la classe Game représentant l'état actuel de la partie.
+        - player_id : identifiant du joueur qui effectue le mouvement, doit être présent dans la partie.
+    
+    Post-conditions : 
+        Retourne un mouvement valide sur le plateau du jeu choisi aléatoirement parmi les mouvements possibles.
     """
 
     directions = possible_move(game, player_id)
     return random.choice(directions)
+
+def exploit(game, player_id, current_state):
+    """
+    Fonction qui renvoie le mouvement choisi par l'IA lors de la phase d'exploitation.
+    
+    Pré-conditions :
+        - game : instance de la classe Game représentant l'état actuel de la partie.
+        - player_id : identifiant du joueur qui effectue le mouvement, doit être présent dans la partie.
+        - current_state : l'état actuel du jeu sous forme d'entier.
+    
+    Post-conditions : 
+        Retourne le meilleur mouvement possible basé sur la Q-table, ou un mouvement aléatoire si current_state n'est pas dans la base de données.
+    """
+    value_state = db.session.query(Qtable).get(current_state)
+    if (current_state):
+        best_action = None
+        best_value = -float('inf')
+        directions = possible_move(game, player_id)
+        for direction in directions:
+            value = value_state[direction]
+            if (value > best_value):
+                best_value = value
+                best_action = direction
+        return best_action
+    else:
+        return explore(game, player_id)
+
+def state(game):
+    """
+    Génère un entier représentant l'état actuel d'un jeu basé sur les informations des joueurs et l'état du plateau.
+
+    Pré-conditions :
+        - game : instance de la classe Game représentant l'état actuel de la partie.
+
+    Post-conditions : 
+        Retourne un entier représentant l'état du jeu, obtenu par la conversion.
+    """
+    current_player = 1 if game.turn_player_1 else 2
+    pos_player_1 = game.pos_player_1.replace(',', '')
+    pos_player_2 = game.pos_player_2.replace(',', '')
+
+    state_str = f"{current_player}{pos_player_1}{pos_player_2}{game.board_state}"
+
+    return int(state_str)
+
+
+def update_q_table(previous_state_move, current_state, current_player_number):
+    """
+    Met à jour la Q-table en fonction de l'état précédent, de l'état actuel et de l'action effectuée.
+
+    Pré-conditions :
+        - previous_state_move : Instance de la classe PreviousStateAction reprécentant l'état précédent du joueur et l'action effectuée.
+        - current_state : Entier représanant l'état actuel du jeu.
+        - current_player_number : Le numéro du joueur actuel (1 ou 2).
+
+    Post-conditions :
+        - Met à jour la Q-table avec la nouvelle valeur calculée pour l'action effectuée.
+    """
+    learning_rate = config.LEARNING_RATE 
+    discount_factor = config.DISCOUNT_FACTOR 
+
+    previous_boardstate = str(previous_state_move[previous_state])[5:]
+    current_boardstate = str(current_state.previous_state)[5:]
+    
+    reward = calculate_reward(previous_boardstate, current_boardstate, current_player_number)  
+    
+    #ajout etat non renconter
+    if  not db.session.query(Qtable).get(previous_state_move.previous_state):
+        new_entry = Qtable(state=previous_state_move.previous_state)
+        db.session.add(new_entry)
+    if  not db.session.query(Qtable).get(current_state):
+        new_entry = Qtable(state=current_state)
+        db.session.add(new_entry)
+    
+    action = previous_state_move.previous_action
+
+    current_q_value = db.session.query(Qtable).get(previous_state_move.previous_state)[action]
+
+    max_future_q_value = max(db.session.query(Qtable).get(current_state).values())
+
+    new_q_value = current_q_value + learning_rate * (reward + discount_factor * max_future_q_value - current_q_value)
+
+    q_table_entry = db.session.query(Qtable).get(previous_state_move.previous_state)
+    q_table_entry[action] = new_q_value
+    db.session.commit()
+
+def calculate_reward(previous_boardstate, current_boardstate, current_player_nb):
+    """
+    Calcule la récompense basée sur la transition d'état.
+
+    Pré-conditions :
+        previous_boardstate : L'état précédent du plateau (sous forme de chaîne).
+        current_boardstate : L'état actuel du plateau (sous forme de chaîne).
+        current_player_nb : Le numéro du joueur actuel (1 ou 2).
+
+    Post-conditions :
+        int : Une récompense calculée, représentant la différence entre le nombre d'éléments pris et perdus.
+    """
+    nb_take = current_boardstate.count(str(current_player_nb)) - previous_boardstate.count(str(current_player_nb))
+    opponent_number = 1 if current_player_nb == 2 else 2
+    nb_lose = current_boardstate.count(str(opponent_number)) - previous_boardstate.count(str(opponent_number))
+    return nb_take - nb_lose
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BOARD_SIZE = 5
 
 #à ajuster
 EPS = 0.9
-GAMMA = 0.5
+DISCOUNT_FACTOR  = 0.5
 LEARNING_RATE = 0.5
 
 basedir = os.path.abspath(os.path.dirname(__file__))
