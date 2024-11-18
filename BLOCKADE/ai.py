4b538f1801fabce9d2e94d54d889caaef047880b
perdita96 <etu51742@henallux.be>
2024-11-18 11:29:45 +0100

    movement pssoble

--- BLOCKADE/ai.py
+++ BLOCKADE/ai.py
@@ -1,41 +1,16 @@
 import random
 
-
-def get_move(game, player_id):
+def possible_move(game, player_id):
     """
-    Fonction qui revoir le mouvement choisit par l'IA
+    Fonction qui revoir les mouvements possible pour l'IA
     Pré-conditions :
-        L'id du joueur qui fait le mouvement doit être l'id d'un joueur présent dans la game
+        - game est la game en cour
+        - L'id du joueur qui fait le mouvement doit être l'id d'un joueur présent dans la game
     Post-conditions : 
-        Retourne un mouvement valide sur la plateau du jeu
-    """
-    # Suggestion : Instancier directions une seule fois et l'importer vu qu'il est aussi utilisé dans la business (par ex dans config)
-    # pour avoir un point de modification unique
-
-    directions = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
-
-    move = random.choice(directions)
-    while not is_move_possible(game, player_id, move) :
-        move = random.choice(directions)
-
-    return move
-
-
-
-def is_move_possible(game, player_id, move) : 
-    """
-    Fonction qui vérifie si le  mouvement est possible
-
-    Pré-conditions :
-        L'id du joueur qui fait le mouvement doit être l'id d'un joueur présent dans la game
-        Le mouvement doit être [ArrowUp,ArrowDown,ArrowLeft,ArrowRight]
-    Post-conditions : 
-        Retourne true ou false en fonction de si le mouvement est valide
+        Retourne la liste des mouvements valides
     """
     board_state = game.board_state
     size = game.size
-    is_possible = True
-
     if player_id == game.player_1_id : 
         current_player = "1"
         current_pos = game.pos_player_1
@@ -44,21 +19,32 @@ def is_move_possible(game, player_id, move) :
         current_pos = game.pos_player_2
 
     x, y = map(int, current_pos.split(","))
+    possible_move = []
+    for move in ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']:
+        match move:
+            case "ArrowUp":
+                new_x, new_y = x - 1, y
+            case "ArrowDown":
+                new_x, new_y = x + 1, y
+            case "ArrowLeft":
+               new_x, new_y = x, y - 1
+            case "ArrowRight":
+                new_x, new_y = x, y + 1
+        if((0 <= new_x < size) and (0 <= new_y < size)): 
+            target_square = board_state[new_x * size + new_y]
+            if(target_square == "0" or target_square == current_player):
+                possible_move.append(move)
+    return possible_move
 
-    match move:
-        case "ArrowUp":
-            new_x, new_y = x - 1, y
-        case "ArrowDown":
-            new_x, new_y = x + 1, y
-        case "ArrowLeft":
-            new_x, new_y = x, y - 1
-        case "ArrowRight":
-            new_x, new_y = x, y + 1
+def get_move(game, player_id):
+    """
+    Fonction qui revoir le mouvement choisit par l'IA
+    Pré-conditions :
+        - game est la game en cour
+        - L'id du joueur qui fait le mouvement doit être l'id d'un joueur présent dans la game
+    Post-conditions : 
+        Retourne un mouvement valide sur la plateau du jeu
+    """
 
-    is_possible =  (0 <= new_x < size) and (0 <= new_y < size)
-
-    if is_possible : 
-        target_square = board_state[new_x * size + new_y]
-        is_possible = (target_square == "0" or target_square == current_player)
-        
-    return is_possible    
+    directions = possible_move(game, player_id)
+    return random.choice(directions)
