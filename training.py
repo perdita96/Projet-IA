fc287258ef9ac2bd4c96a4a80c41ab32e63f02f4
perdita96 <etu51742@henallux.be>
2024-12-03 17:04:14 +0100

    commantaire training

--- training.py
+++ training.py
@@ -20,7 +20,7 @@ def play_game(game) :
 
 if __name__ == "__main__":
 
-    nb_games_wanted = input("Combien de partie voulez-vous jouer : ")
+    nb_games_wanted = int(input("Combien de partie voulez-vous jouer : ")) # j'ai ajouter la converssion
 
     #démarrer l'ap ==> BASE DE DONNEES
     app.run()
@@ -31,7 +31,8 @@ if __name__ == "__main__":
     ai_id = id_searched_player('IA')
 
     #si parti non terminée -> reprend et finir
-    last_game = db.session.query(Game).order_by(Game.id.desc()).first()
+    #last_game = db.session.query(Game).order_by(Game.id.desc()).first()
+    last_game = db.session.query(Game).filter(Game.player_1 == ai_id, Game.player_2 == ai_id).first() # c'est pas mieux?
 
     if last_game : 
         if last_game.winner == 0 : 
@@ -50,6 +51,8 @@ if __name__ == "__main__":
         play_game(new_game)
 
         i_game += 1
+
+    #si nb_games_played = MAX_GAMES -> lancer l'évaluation contre elle même et contre random
 
 
 
